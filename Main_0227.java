7ad347ecd39c3fb8f18b45f52bdff1178fbc4111
user name <duthd3@naver.com>
2021-02-27 13:55:01 +0900

    21.02.27 study(java)

---
+++ Main_0227.java
@@ -0,0 +1,149 @@
+import java.io.IOException;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+//		System.out.print("Hello ");
+//		System.out.println("World");
+//		System.out.println("Hello Java");
+		
+//		System.out.print("Hello world. Hello Java\n");
+//		System.out.println("Hello \nworld. \nHello Java");
+//		System.out.println("Hello \tworld. \tHello Java");
+//		System.out.println("123456781234567812345678123456781234567812345678");
+//		System.out.println("1\t123\t1234567\t12345678\t1");		
+	
+//		int i = 10;
+//		char c = 'A';
+//		float f = 10.1f;
+//		String str = "Hello World";
+//		
+//		System.out.printf("i=%d\n", i);
+//		System.out.printf("f=%f\n", f);
+//		System.out.printf("c=%c\n", c);
+//		System.out.printf("str=%s\n", str);
+		
+//		int i = 10;
+//		
+//		System.out.printf("i=%d\n", i);
+//		
+//		System.out.print("입력: ");
+//		i = System.in.read();
+//		System.out.print("출력: ");
+//		System.out.println(i);
+		
+//		String message;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("문자열 입력: ");
+//		message = sc.next();
+//		System.out.println(message);
+//		
+//		message = sc.next();
+//		System.out.println(message);
+		
+//		String message;
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("문자열 입력: ");
+//		message = sc.nextLine();
+//		System.out.println(message);
+//		System.out.print("문자열 입력: ");
+//		message = sc.nextLine();
+//		System.out.println(message);
+
+//		Scanner sc = new Scanner(System.in);
+//		int i = 10;
+//		
+//		System.out.printf("i=%d\n", i);
+//		
+//		System.out.print("입력: ");
+//		i = sc.nextInt();
+//		System.out.print("출력: ");
+//		System.out.println(i);
+		
+//		Scanner sc = new Scanner(System.in);
+//		int age;
+//		String name;	
+//		
+//		System.out.print("이름 입력: ");
+//		name = sc.nextLine();
+//		
+//		System.out.print("나이 입력: ");
+//		age = sc.nextInt();
+//		
+//		
+//		System.out.println("이름: " + name);
+//		System.out.println("나이: " + age);
+		
+//		Scanner sc = new Scanner(System.in);
+//		int jsp, java, total;
+//		
+//		float average;
+//		String name;
+//		
+//		System.out.print("이름: ");
+//		name = sc.nextLine();
+//		
+//		System.out.print("자바점수: ");
+//		java = sc.nextInt();
+//		
+//		System.out.print("jsp점수: ");
+//		jsp = sc.nextInt();
+//		
+//		
+//		
+//		total = java + jsp;
+//		average = total /(float)2;
+//	
+//		
+//		System.out.printf("\n[%s님의 성적] \n", name);
+//		System.out.printf("자바: %d 점\n", java);
+//		System.out.printf("jsp: %d 점\n", jsp);
+//		System.out.printf("합계 %d 점\n", total);
+//		System.out.printf("평균 %.1f 점\n", average);
+		
+//		int a=5, b=10;
+//		int c;
+//		
+//		c = a+b;
+//		System.out.println(a+ "+" + b + "=" + c);
+//		
+//		int share = 10 / 8;
+//		int remain = 10 % 8;
+//		
+//		System.out.println(10 + "/" + 8 +"=" + share);
+//		System.out.println(10 + "%" + 8 + "=" + remain);
+		
+//		System.out.println(10 < 5);
+//		System.out.println(1 < 5);
+//		System.out.println(3.14 == 3);
+//		System.out.println(3.0 == 3);
+//		System.out.println(3.0f != 3.0);
+		
+//		String str = "Hello World";
+//		String sstr = new String("Hello World");
+//		
+//		System.out.println(str == sstr);
+//		System.out.println(str.equals(sstr));
+		
+		
+		int x = 10;
+		int y = -10;
+		
+		int absX = (x>=0) ? x : -x;
+		int absY = (y>=0) ? y : -y;
+		
+		System.out.println(absX);
+		System.out.println(absY);
+		
+		
+		
+		
+		
+		
+	}
+}
+
+
