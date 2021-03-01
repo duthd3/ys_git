ae3abc6b11de999f24b967e6af4e8ca53f3ea94f
user name <duthd3@naver.com>
2021-03-01 21:34:56 +0900

    21.03.01 study(java)

---
+++ CarTest2.java
@@ -0,0 +1,31 @@
+/*class Car{
+	String color;
+	String gearType;
+	int door;
+	
+	Car(){
+		this("white", "auto", 4);
+	}
+	
+	Car(String color){
+		this(color, "auto", 4);
+	}
+	Car(String color, String gearType, int door){
+		this.color =color;
+		this.gearType = gearType;
+		this.door = door;
+	}
+}
+public class CarTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1 = new Car();
+		Car c2 = new Car("blue");
+		
+		System.out.println("c1의 color = " + c1.color + ", c1의 gearType = " + c1.gearType + ", c1의 door = " + c1.door);
+		System.out.println("c2의 color = " + c2.color + ", c2의 gearType = " + c2.gearType + ", c2의 door = " + c2.door);
+	}
+
+}
+*/
